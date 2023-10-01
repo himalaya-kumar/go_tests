eebaea0a267e0248caa34ac611047169c7d10915
himalaya-kumar <itshimalaykumarx@gmail.com>
2023-10-01 14:58:39 +0530

    integer

--- nteger/nteger_test.go
+++ nteger/nteger_test.go
@@ -3,23 +3,28 @@ package nteger
 import "testing"
 
 func TestAdder(t *testing.T) {
+	type value struct {
+		x int
+		y int
+	}
 
-	t.Run("2 + 2", func(t *testing.T) {
-		sum := Add(2, 2)
-		expected := 4
-		if sum != expected {
-			t.Errorf("expected '%d' but got '%d'", expected, sum)
-		}
-
-	})
-
-	t.Run("5 + 1", func(t *testing.T) {
-		sum := Add(5, 1)
-		expected := 6
-		if sum != expected {
-			t.Errorf("expected '%d' but got '%d'", expected, sum)
-		}
-
-	})
+	type testsStruct struct {
+		name string
+		val  value
+		want int
+	}
+	tests := []testsStruct{
+		{"2 + 2", value{x: 2, y: 2}, 4},
+		{"5 + 1", value{x: 5, y: 1}, 6},
+		{"5 + 4", value{x: 5, y: 4}, 9},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := Add(test.val.x, test.val.y)
+			if got != test.want {
+				t.Errorf("expected '%d' but got '%d'", test.want, got)
+			}
+		})
+	}
 
 }
