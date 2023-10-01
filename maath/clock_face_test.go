5945d7acbb7c9b5c451b6799aa00f2c5ee47f9f4
himalaya-kumar <itshimalayakumarx@gmail.com>
2023-10-02 00:59:34 +0530

    coding

--- maath/clock_face_test.go
+++ maath/clock_face_test.go
@@ -33,11 +33,12 @@ func TestSecondHandVector(t *testing.T) {
 		point Point
 	}{
 		{simpleTime(0, 0, 30), Point{0, -1}},
+		{simpleTime(0, 0, 45), Point{-1, 0}},
 	}
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
 			got := secondHandInPoints(c.time)
-			if got != c.point {
+			if !roughlyEqualPoints(got, c.point) {
 				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
 			}
 		})
@@ -51,4 +52,13 @@ func simpleTime(hours, mintues, seconds int) time.Time {
 
 func testName(t time.Time) string {
 	return t.Format("15:04:05")
+}
+
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoints(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) && roughlyEqualFloat64(a.Y, b.Y)
 }
--- maath/maath.go
+++ maath/maath.go
@@ -19,6 +19,11 @@ func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
 
-func secondHandInPoints(time time.Time) Point {
-	return Point{0, -1}
+func secondHandInPoints(t time.Time) Point {
+
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
 }
