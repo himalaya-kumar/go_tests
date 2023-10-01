a5787e54e0424873dc1e58516f9c6c1369f6d1c2
himalaya-kumar <itshimalayakumarx@gmail.com>
2023-10-02 00:09:26 +0530

    added test cases

--- maath/clock_face_test.go
+++ maath/clock_face_test.go
@@ -7,13 +7,41 @@ import (
 )
 
 func TestSecondsInRadians(t *testing.T) {
-	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
-	want := math.Pi
-	got := secondsInRadians(thirtySeconds)
-	if got != want {
-		t.Fatalf("Wanted %v radians, but got %v", want, got)
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
+	for _, c := range cases[:0] {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians,but got %v", c.angle, got)
+			}
+		})
 	}
 }
+
+func simpleTime(hours, mintues, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, mintues, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
+
+// func TestSecondsInRadians(t *testing.T) {
+// 	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+// 	want := math.Pi
+// 	got := secondsInRadians(thirtySeconds)
+// 	if got != want {
+// 		t.Fatalf("Wanted %v radians, but got %v", want, got)
+// 	}
+// }
 
 func TestSecondHandAtMidnight(t *testing.T) {
 
