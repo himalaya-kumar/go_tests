9ad6ee2673dfdc67a16143e7933c2b7ded5adeaa
himalaya-kumar <itshimalaykumarx@gmail.com>
2023-10-01 14:32:47 +0530

    integer working

--- nteger/nteger.go
+++ nteger/nteger.go
@@ -1,5 +1,6 @@
 package nteger
 
+// Add function :add values of two number
 func Add(a, b int) int {
 	return a + b
 }
--- nteger/nteger_test.go
+++ nteger/nteger_test.go
@@ -4,9 +4,22 @@ import "testing"
 
 func TestAdder(t *testing.T) {
 
-	sum := Add(2, 2)
-	expected := 4
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
-	}
+	t.Run("2 + 2", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+
+	})
+
+	t.Run("5 + 1", func(t *testing.T) {
+		sum := Add(5, 1)
+		expected := 6
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+
+	})
+
 }
