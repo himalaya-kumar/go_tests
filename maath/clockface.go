5ef4295ba14f5f75027659212780cf364c0c7f74
himalaya-kumar <itshimalayakumarx@gmail.com>
2023-10-02 19:26:07 +0530

    clockface at some point

--- maath/clockface.go
+++ maath/clockface.go
@@ -8,35 +8,39 @@ import (
 	"time"
 )
 
-// A Point represent two dimmensional Cartesian plane
-type Point struct {
-	X float64 `yml:"x"`
-	Y float64 `yml:"y"`
-}
-type Svg struct {
-	XMLName xml.Name `xml:"svg"`
-	Text    string   `xml:",chardata"`
-	Xmlns   string   `xml:"xmlns,attr"`
-	Width   string   `xml:"width,attr"`
-	Height  string   `xml:"height,attr"`
-	ViewBox string   `xml:"viewBox,attr"`
-	Version string   `xml:"version,attr"`
-	Circle  struct {
-		Text  string `xml:",chardata"`
-		Cx    string `xml:"cx,attr"`
-		Cy    string `xml:"cy,attr"`
-		R     string `xml:"r,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"circle"`
-	Line []struct {
-		Text  string `xml:",chardata"`
-		X1    string `xml:"x1,attr"`
-		Y1    string `xml:"y1,attr"`
-		X2    string `xml:"x2,attr"`
-		Y2    string `xml:"y2,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"line"`
-}
+type (
+
+	// A Point represent two dimmensional Cartesian plane
+	Point struct {
+		X float64 `yml:"x"`
+		Y float64 `yml:"y"`
+	}
+
+	Svg struct {
+		XMLName xml.Name `xml:"svg"`
+		Text    string   `xml:",chardata"`
+		Xmlns   string   `xml:"xmlns,attr"`
+		Width   string   `xml:"width,attr"`
+		Height  string   `xml:"height,attr"`
+		ViewBox string   `xml:"viewBox,attr"`
+		Version string   `xml:"version,attr"`
+		Circle  Circle   `xml:"circle"`
+		Line    []Line   `xml:"line"`
+	}
+
+	Circle struct {
+		Cx float64 `xml:"cx,attr"`
+		Cy float64 `xml:"cy,attr"`
+		R  float64 `xml:"r,attr"`
+	}
+
+	Line struct {
+		X1 float64 `xml:"x1,attr"`
+		Y1 float64 `xml:"y1,attr"`
+		X2 float64 `xml:"x2,attr"`
+		Y2 float64 `xml:"y2,attr"`
+	}
+)
 
 const (
 	secondHandLength = 90
