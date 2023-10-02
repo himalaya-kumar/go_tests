7b7deeeb251d7c2e0d28464742e018e588568e5b
himalaya-kumar <itshimalayakumarx@gmail.com>
2023-10-02 19:00:40 +0530

    clockface at some point

--- maath/clockface.go
+++ maath/clockface.go
@@ -77,7 +77,7 @@ func secondHand(w io.Writer, t time.Time) {
 	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // Scale
 	p = Point{p.X, -p.Y}                                      // Flip
 	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         //translate
-	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%f" y2="%f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
 const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
